2f294842389ad92218344f955f4f174bef738b3f
VargaOrsi <orsi0120@gmail.com>
2022-04-20 18:03:15 +0200

    04.19. orai anyag

---
+++ teglalapKerTer.js
@@ -0,0 +1,6 @@
+var szam1 = 5;
+var szam2 = 10;
+var kerulet = 2 * (szam1 + szam2);
+var terulet = szam1 * szam2;
+document.write("A téglalap kerülete:" + kerulet);
+document.write("A téglalap területe:" + terulet);
---
+++ teglalapKerTer.ts
@@ -0,0 +1,6 @@
+var szam1: number = 5;
+var szam2: number = 10;
+var kerulet: number = 2 * (szam1 + szam2);
+var terulet: number = szam1 * szam2;
+document.write("A téglalap kerülete:" + kerulet);
+document.write("A téglalap területe:" + terulet);
